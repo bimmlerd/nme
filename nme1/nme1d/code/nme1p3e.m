8cf8c8adc7d32c9659fe54c5654e7f0846d8ae47
David Bimmler <bimmlerd@student.ethz.ch>
2015-09-20 16:57:20 +0200

    don't be an idiot

--- nme1/nme1d/code/nme1p3e.m
+++ nme1/nme1d/code/nme1p3e.m
@@ -1,5 +1,7 @@
-res = zeros(1,5);
 tt = zeros(3,5);
+tt2 = zeros(3,5);
+res = zeros(1,5);
+res2 = zeros(1,5);
 scale = 2 .^(2:6);
 for i = 1:5
     n = 2^(i+1);
@@ -10,10 +12,17 @@ for i = 1:5
         tic
         y = reshape( B * reshape(x,n,n) * A', n*n, 1 );
         tt(t, i) = toc;
+        tic
+        y = kron(A,B) * x;
+        tt2(t, i) = toc;
     end
-    res(i) = min(tt(:,i)); 
+    res(i) = min(tt(:,i));
+    res2(i) = min(tt2(:,i));
 end
 
-plot(scale, res);
+semilogy(scale, res);
+hold on;
+semilogy(scale, res2);
 xlabel('n');
 ylabel('time');
+legend('reshape', 'kron', 'Location', 'east');
