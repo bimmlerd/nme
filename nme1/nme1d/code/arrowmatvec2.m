9f3fe351868774e7b5befb95d69ea12170bbbf44
David Bimmler <bimmlerd@student.ethz.ch>
2015-09-22 11:30:49 +0200

    arrowmatvec2 and comparison

--- nme1/nme1d/code/arrowmatvec2.m
+++ nme1/nme1d/code/arrowmatvec2.m
@@ -1,6 +1,7 @@
 function y = arrowmatvec2(d,a,x)
 if (length(d) ~= length(a)), error ('size mismatch'); end
-A = [diag(d(1:end-1).^2+a(1:end-1).^2),(d(1:end-1) + d(4)) .* a(1:end-1)];
-r
-A = A + tril(A',-1); % make symmetric
-y = A * x;
+a(end) = 0;
+yy = d .* x + x(end) * a;
+yy(end) = yy(end) + dot(a(1:end-1),x(1:end-1));
+y = d .* yy + yy(end) * a;
+y(end) = y(end) + dot(a(1:end-1),yy(1:end-1));
---
+++ nme1/nme1d/code/nme1p1e.m
@@ -0,0 +1,31 @@
+scale = 2 .^ (5:12);
+c = length(scale);
+tries = 1;
+res = zeros(c, 2);
+tt = zeros(2,tries);
+for i = 1:c
+    n = scale(i);
+    a = rand(n,1);
+    d = rand(n,1);
+    x = rand(n,1);
+    for t = 1:tries
+        tic
+        y = arrowmatvec(d,a,x);
+        tt(1,t) = toc;
+        tic
+        yy = arrowmatvec2(d,a,x);
+        tt(2,t) = toc;
+        if (y ~= yy)
+            error ('mismatch');
+        end
+    end
+    res(i,1) = min(tt(1,:));
+    res(i,2) = min(tt(2,:));
+end
+
+loglog(scale, res(:,1));
+hold on;
+loglog(scale, res(:,2));
+xlabel('n');
+ylabel('time');
+legend('arrowmatvec', 'arrowmatvec2', 'Location', 'east');
