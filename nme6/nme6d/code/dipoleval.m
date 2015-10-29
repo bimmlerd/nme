a2ea94bbdd1fc05172c7b9cf272ee9bb70a9c74c
David Bimmler <bimmlerd@student.ethz.ch>
2015-10-29 14:47:09 +0100

    vectorize dipoleval

--- nme6/nme6d/code/dipoleval.m
+++ nme6/nme6d/code/dipoleval.m
@@ -1,14 +1,15 @@
 function dp = dipoleval( t, y, x )
 % Returns the row vector (p'(x)) for a p interpolating (t,y)
-y_prime = zeros(1, length(y));
+y_prime = zeros(length(x), length(y));
+y = repmat(y, length(x), 1);
 for i = 1:length(y)
     for k=i-1:-1:1
         div = t(i)-t(k);
-        thing = (y(k+1)-y(k));
+        thing = (y(:, k+1)-y(:, k));
         thing2 = x-t(i);
-        y_prime(k) = y_prime(k+1) + (thing + thing2*(y_prime(k+1) - y_prime(k)))/div;
-        y(k) = y(k+1)+thing*thing2/div;
+        y_prime(:, k) = y_prime(:, k+1) + (thing + thing2' .* (y_prime(:, k+1) - y_prime(:, k)))/div;
+        y(:, k) = y(:, k+1)+(thing.*thing2')/div;
     end
 end
-dp = y_prime(1);
+dp = y_prime(:, 1);
 end
